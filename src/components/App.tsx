b3316380548ebcae2b534d025dc967a278cf33f3
Fraser Provan <FraserProvan2@gmail.com>
2019-07-23 22:05:17 +0100

    option auto finds pokemon by ID

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,64 +1,55 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 // Components
-import SearchPokemon from './children/SearchPokemon';
+import SearchPokemon from "./children/SearchPokemon";
 
 // SCSS
-import './../scss/variables.scss';
-import './../scss/bootstrap.scss';
+import "./../scss/variables.scss";
+import "./../scss/bootstrap.scss";
 
 // Models
-import PokemonModal from '../models/Pokemon';
-const Pokemon = new PokemonModal;
+import PokemonModel from "../models/Pokemon";
+const Pokemon = new PokemonModel();
 
 class App extends Component {
-
   constructor(props: any) {
     super(props);
 
     this.state = {
       pokemonData: {},
-      error: "",
+      error: ""
     };
 
     // binding functions
-    this.handleClick = this.handleClick.bind(this);
-  } 
+    this.setPokemonDataById = this.setPokemonDataById.bind(this);
+  }
 
   render() {
     return (
       <div className="App">
         <div className="row justify-content-center">
-
           {/* Search Box */}
           <div className="col-md-8">
-            <SearchPokemon 
-                forChildOnClick={this.handleClick}
+            <SearchPokemon
+              setById={this.setPokemonDataById}
             />
           </div>
-
         </div>
       </div>
     );
   }
-  
+
   // Methods
 
-  handleClick() {
-    console.log('Click happened');
-  }
-  
-  setPokemonDataById(id: number) {
+  setPokemonDataById = (id: number): any => {
     return Pokemon.getById(id)
-    .then((pokemonData: any) => {
-      this.setState({ pokemonData });
-    })
-    .catch((error: any) => {
-      this.setState({ error });
-    });
-  }
-  
-  
+      .then((pokemonData: any) => {
+        this.setState({ pokemonData });
+      })
+      .catch((error: any) => {
+        this.setState({ error });
+      });
+  };
 }
 
 export default App;
--- src/components/children/SearchPokemon.tsx
+++ src/components/children/SearchPokemon.tsx
@@ -1,29 +1,39 @@
-import React from 'react';
+import React from "react";
 
 interface Props {
-  forChildOnClick: () => void;
+  setById: (id: number) => any,
 }
 
-interface State {}
+interface State {
+  pokemonNumber: 1,
+}
 
 class SearchPokemon extends React.Component<Props, State> {
   constructor(props: any) {
     super(props);
 
-    console.log(props.function);
-  } 
+    this.state = {
+      pokemonNumber: 1
+    };
+  }
 
   render() {
     return (
       <div className="card">
         <div className="card-body">
           <h6>Search Pokémon</h6>
-          
+
           {/* Input */}
           <div className="form-row mb-2">
             <div className="col-3">
-              <select className="form-control text-center">
-                <option className="form-control">1</option>
+              <select
+                className="form-control text-center"
+                onChange={this.updatePokemonNumber.bind(this)}
+                value={this.state.pokemonNumber}
+              >
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
               </select>
             </div>
             <div className="col-9">
@@ -34,24 +44,24 @@ class SearchPokemon extends React.Component<Props, State> {
               />
             </div>
           </div>
-
-          {/* Submit */}
-          <div className="row">
-            <div className="col-12">
-              <button
-                type="button"
-                className="btn btn-primary btn-lg btn-block"
-                onClick={this.props.forChildOnClick}
-              >
-                Go
-              </button>
-            </div>
-          </div>
-
+          
         </div>
       </div>
     );
   }
+
+  updatePokemonNumber(e: any) {
+    this.setState({
+      pokemonNumber: e.target.value
+    });
+
+    this.props.setById(this.state.pokemonNumber);
+  }
 }
 
 export default SearchPokemon;
+
+// TODO
+// 1. WHEN MOUNTED, automatically call setById to 1
+// 2. for loop to calc select number 1-151
+// 3. if pokemonData prop (to be passed) is set, use this value for pokemon number/name
