29f2162ed1e668aa66b2bb8550cea676a61e31f9
Fraser Provan <FraserProvan2@gmail.com>
2019-07-25 22:38:52 +0100

    auto suggest from json file

--- src/components/search/_SearchInput.tsx
+++ src/components/search/_SearchInput.tsx
@@ -1,27 +1,100 @@
 import React from "react";
 import Utility from "../../helpers/Utility";
 
+// AutoSuggest Lib
+import Autosuggest from "react-autosuggest";
+import "./../../scss/autosuggest.scss";
+
+// data to AutoSuggest
+import pokemonNames from "./../../pokemonNames.json";
+
 interface Props {
   name: string;
 }
 
-interface State {}
+interface State {
+  value: string;
+  suggestions: any[];
+}
 
 class SearchInput extends React.Component<Props, State> {
   constructor(props: any) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      value: Utility.ucFirst(this.props.name)
+        ? Utility.ucFirst(this.props.name)
+        : "",
+      suggestions: []
+    };
   }
 
   render() {
+    const { value, suggestions } = this.state;
+    const inputProps = {
+      placeholder: Utility.ucFirst(this.props.name)
+        ? Utility.ucFirst(this.props.name)
+        : "",
+      value,
+      onChange: this.onChange
+    };
+
     return (
-      <input
-        className="form-control text-center"
-        type="text"
-        value={Utility.ucFirst(this.props.name)}
+      <Autosuggest
+        suggestions={suggestions}
+        onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+        onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+        getSuggestionValue={this.getSuggestionValue}
+        renderSuggestion={this.renderSuggestion}
+        inputProps={inputProps}
       />
     );
+  }
+
+  onChange = (event: any, { newValue, method }: any) => {
+    this.setState({
+      value: newValue
+    });
+  };
+
+  onSuggestionsFetchRequested = ({ value }: any) => {
+    this.setState({
+      suggestions: this.getSuggestions(value)
+    });
+  };
+
+  onSuggestionsClearRequested = () => {
+    this.setState({
+      suggestions: []
+    });
+  };
+
+  escapeRegexCharacters(str: string) {
+    return str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+  }
+
+  getSuggestions(value: string) {
+    const escapedValue = this.escapeRegexCharacters(value.trim());
+
+    if (escapedValue === "") {
+      return [];
+    }
+
+    const regex = new RegExp("^" + escapedValue, "i");
+
+    return pokemonNames.filter(language => regex.test(language.name));
+  }
+
+  getSuggestionValue(suggestion: any) {
+    alert(suggestion.name);
+
+    // SENT TO FUNCTION HERE
+
+    return "";
+  }
+
+  renderSuggestion(suggestion: any) {
+    return <span>{suggestion.name}</span>;
   }
 }
 
